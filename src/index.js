523e1b55bc758635570b45a9dec18716ce2c3101
Sergecage <34719013+Sergecage@users.noreply.github.com>
2021-03-13 18:45:27 +0300

    feat:add functions to find max, min and aver

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
+  if (typeof array != 'undefined' && array.length > 0) {
+    return Math.min(...array)
+  } else {
   return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array != 'undefined' && array.length > 0) {
+    return Math.max(...array)
+  } else {
+      return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof array != 'undefined' && array.length > 0)  {
+    var arr = 0
+    for ( i =0; i <array.length; i++) {
+      arr+=array[i]
+    }
+    return arr / array.length
+  } else {
+      return 0;
+  }
 }
